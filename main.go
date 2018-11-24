e3a31380f206ea6b47b6e810fbaa9445a4fbccb7
Grigory Zubankov <Grigory.Zubankov@acronis.com>
2018-11-24 14:00:04 +0300

    add: --color option

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/ssgreg/logftext"
 )
 
 func main() {
-	signal.Ignore(os.Interrupt)
-	ok := logftext.EnableSeqTTY(os.Stdout, true)
-	noColor := !ok || logftext.CheckNoColor()
+	coloredLogs := flag.String("color", "auto", `Show colored logs ("always"|"never"|"auto"). --color= is the same as --color=always.`)
+	flag.Parse()
 
-	scan(os.Stdin, noColor)
+	signal.Ignore(os.Interrupt)
+	scan(os.Stdin, handleColorOption(*coloredLogs))
+}
+
+// handleColorOption handles 'color' option. It returns true if colored
+// output should be turned off.
+func handleColorOption(coloredLogs string) bool {
+	force := false
+
+	switch strings.ToLower(coloredLogs) {
+	case "never":
+		return true
+
+	case "always", "":
+		force = true
+
+		fallthrough
+
+	default:
+		ok := logftext.EnableSeqTTY(os.Stdout, true)
+		return !force && (!ok || logftext.CheckNoColor())
+	}
 }
